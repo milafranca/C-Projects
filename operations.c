a93dd19a3776bb001fc2dc14e2325d0aed184166
milafranca <147221877+milafranca@users.noreply.github.com>
2025-08-31 14:41:21 -0300

    Add files via upload

---
+++ operations.c
@@ -0,0 +1,17 @@
+#include "operations.h"
+
+float sum(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float multiply(float a, float b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
---
+++ operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+float sum(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+
+#endif
